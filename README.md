dd4df84f4bcca17b7540bb0346fb7670fc7e875a
Milton Jacomini <miltonjacomini@gmail.com>
2020-02-01 16:25:58 -0200

    Add main project files

---
+++ README.md
@@ -0,0 +1,16 @@
+# Desafio para a empresa Peixe Urbano
+
+## Sobre
+Foi desenvolvida a aplicacao como proposta, todos os endpoints estão cobertos por testes e o
+serviço principal também foi.
+
+As tecnologias utilizadas foram o pacote Spring (Mvc, Data) com bando da dados em memória H2.
+
+## Instruções
+
+Para compilar o programa basta executar o maven:
+`mvn clean install`
+
+Para subir o projeto
+`java -jar target/desafio.jar`
+
---
+++ src/main/java/br/com/peixe/desafio/DesafioApplication.java
@@ -0,0 +1,66 @@
+package br.com.peixe.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@SpringBootApplication
+@EnableSwagger2
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.peixe"))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiEndPointInfo());
+	}
+
+	private ApiInfo apiEndPointInfo() {
+		return new ApiInfoBuilder().title("Peixe Urbano API")
+				.description("Peixe Urbano REST API")
+				.contact(new Contact("Milton Jacomini", "http://miltonjneto.com.br", "miltonjacomini@gmail.com"))
+				.version("0.0.1")
+				.build();
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
